e9862d07a76d6f3ac16eefc8ddd515380534cc04
mateusz.konieczny@hotmail.com <Javovanie576^%#*>
2022-11-20 22:03:10 +0100

    -initial commit

--- src/main/java/com/example/idiom/IdiomHandling.java
+++ src/main/java/com/example/idiom/IdiomHandling.java
@@ -4,15 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 public class IdiomHandling {
+
+
     final String idiomUri = "https://www.ang.pl/slownictwo/idiomy/";
     private String englishWordBegin = "class=\"sm2_button\">play</a>";
     private String polishWordBegin = "<tr><td><span class=\"pl_flag\">tłumaczenie:</span></td><td class=\"pol\">";
@@ -23,12 +20,13 @@ public class IdiomHandling {
 
     @GetMapping("/idiom")
     public void getEnglishMeaning() {
-
-//        for (int i = 1; i <= 521; i++) {
-//            idiomsArrayList.add(getOneIdiom(i));
-//        }
-        idiomsArrayList.add(getOneIdiom(1));
-        idiomsArrayList.add(getOneIdiom(2));
+        int idiomLenght=521;
+        idiomsArrayList.ensureCapacity(idiomLenght);
+        for (int i = 1; i <= idiomLenght; i++) {
+            idiomsArrayList.add(getOneIdiom(i));
+        }
+        CsVWriter csVWriter = new CsVWriter();
+        csVWriter.save(idiomsArrayList);
 
 
     }
@@ -52,7 +50,7 @@ public class IdiomHandling {
         idioms.setId(httpEnd);
         System.out.println(idioms);
         return idioms;
-
     }
+
 
 }
