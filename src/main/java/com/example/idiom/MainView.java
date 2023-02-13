01414e3a5e9c645ce53efb23d6913d7965725429
Mateusz <pelzu12345@gmail.com>
2023-02-13 18:44:02 +0100

    -Add vaadin dependence and MainView.java

---
+++ src/main/java/com/example/idiom/MainView.java
@@ -0,0 +1,17 @@
+package com.example.idiom;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    
+    
+    public MainView() {
+        add(new Text("Welcome to English-Idiom App"));
+
+    }
+
+}
