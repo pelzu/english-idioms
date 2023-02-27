5d5d1cefac827e037eee6223ad31ec6ea08046eb
Mateusz <pelzu12345@gmail.com>
2023-02-28 00:51:55 +0100

    -Implementing PaginationInterface in  IdiomPagination and PhrasalPagination class

--- src/main/java/com/example/idiom/service/idiom/IdiomPagination.java
+++ src/main/java/com/example/idiom/service/idiom/IdiomPagination.java
@@ -1,15 +1,18 @@
 package com.example.idiom.service.idiom;
 
-import com.example.idiom.service.approach.HtmlPaginationScrapper;
+import com.example.idiom.service.approach.pagination.PaginationScrapper;
+import com.example.idiom.service.approach.pagination.PaginationInterface;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class IdiomPagination {
+public class IdiomPagination implements PaginationInterface {
 
     private final String IDIOM_BASE_LINK = "https://www.ang.pl/slownictwo/idiomy/page/1";
-    public int getNumberOfPageIdiom() {
-        return   HtmlPaginationScrapper.getNumberOfPage(IDIOM_BASE_LINK);
-    }
 
+
+    @Override
+    public int getNumberOfPage() {
+        return PaginationScrapper.getNumberOfPage(IDIOM_BASE_LINK);
+    }
 }
 
--- src/main/java/com/example/idiom/service/phrasal/PhrasalPagination.java
+++ src/main/java/com/example/idiom/service/phrasal/PhrasalPagination.java
@@ -1,16 +1,15 @@
 package com.example.idiom.service.phrasal;
 
-import com.example.idiom.service.approach.HtmlPaginationScrapper;
+import com.example.idiom.service.approach.pagination.PaginationScrapper;
+import com.example.idiom.service.approach.pagination.PaginationInterface;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class PhrasalPagination {
+public class PhrasalPagination implements PaginationInterface {
     private final String PHRASAL_VERB_BASE_LINK = "https://www.ang.pl/slownictwo/phrasal-verbs/page/1";
 
-    public int getNumberOfPagePhrasalVerb() {
-        return   HtmlPaginationScrapper.getNumberOfPage(PHRASAL_VERB_BASE_LINK);
-
-
-
+    @Override
+    public int getNumberOfPage() {
+        return  PaginationScrapper.getNumberOfPage(PHRASAL_VERB_BASE_LINK);
     }
 }
