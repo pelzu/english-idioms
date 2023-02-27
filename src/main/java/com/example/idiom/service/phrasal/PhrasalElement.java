0a9da3b17365f37178365d8e461b8525c2e76642
Mateusz <pelzu12345@gmail.com>
2023-02-28 00:58:41 +0100

    -Implementing  ElementScrapperInterface in IdiomElement.java and PhrasalElement.java.

--- src/main/java/com/example/idiom/service/phrasal/PhrasalElement.java
+++ src/main/java/com/example/idiom/service/phrasal/PhrasalElement.java
@@ -1,16 +1,8 @@
 package com.example.idiom.service.phrasal;
 
+import com.example.idiom.service.approach.elements.ElementScrapper;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 @Slf4j
 public class PhrasalElement {
@@ -23,37 +15,9 @@ public class PhrasalElement {
         this.phrasalPagination = phrasalPagination;
     }
 
-    public Elements getElements() {
+    public Elements getPhrasalElements() {
 
-        int paginationNumbers = this.phrasalPagination.getNumberOfPagePhrasalVerb();
-        ExecutorService executorService = Executors.newFixedThreadPool(30);
-        Elements elements = new Elements();
-        List<Future<Elements>> futureElements = new ArrayList<>();
-
-        for (int i = 1; i <= paginationNumbers; i++) {
-            int increment = i;
-            Callable<Elements> task = () -> {
-                try {
-                    log.info("Downloading HTML site:" + PHRASAL_VERB_LINK + increment);
-                    return Jsoup.connect(PHRASAL_VERB_LINK + increment).get().select("div[class=row border-bottom py-2]");
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            };
-            futureElements.add(executorService.submit(task));
-        }
-        for (Future<Elements> element : futureElements
-        ) {
-            try {
-                elements.addAll(element.get());
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-
-        }
-
-        executorService.shutdown();
-        return elements;
+        return ElementScrapper.getElements(PHRASAL_VERB_LINK, phrasalPagination);
     }
 
 
