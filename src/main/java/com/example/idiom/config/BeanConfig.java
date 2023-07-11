1c2db1ed8cb0d3e4ddac2521a6ca83cabfc108a3
Mateusz <pelzu12345@gmail.com>
2023-07-11 23:56:16 +0200

    -add Comment

--- src/main/java/com/example/idiom/config/BeanConfig.java
+++ src/main/java/com/example/idiom/config/BeanConfig.java
@@ -14,7 +14,7 @@ import com.example.idiom.service.phrasal.PhrasalVerbsParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+//Loli
 @Configuration
 public class BeanConfig {
 
